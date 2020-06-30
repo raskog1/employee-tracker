218144ae83f0e9496a85b18764d1c341bea0c91b
Ryan Skog <ryan.skog@outlook.com>
2020-06-30 13:44:58 -0700

    Updated schema and seeds.sql

--- schema.sql
+++ schema.sql
@@ -9,23 +9,38 @@ CREATE TABLE department
     AUTO_INCREMENT,
     name VARCHAR
     (30) NOT NULL,
-)
+    PRIMARY KEY
+    (id)
+);
 
     CREATE TABLE role
     (
-        id INT NOT NULL,
-        title VARCHAR(30) NOT NULL,
-        salary DECIMAL,
-        department_id INT NOT NULL,
-        PRIMARY KEY (id)
-    )
+        id INT NOT NULL
+        AUTO_INCREMENT,
+    title VARCHAR
+        (30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY
+        (id),
+    FOREIGN KEY
+        (department_id) REFERENCES department
+        (id)
+);
 
-    CREATE TABLE employee
-    (
-        id INT NOT NULL,
-        first_name VARCHAR(30) NOT NULL,
-        last_name VARCHAR(30) NOT NULL,
-        role_id INT NOT NULL,
-        manager_id INT NULL,
-        PRIMARY KEY (id)
-    )
+        CREATE TABLE employee
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT,
+    first_name VARCHAR
+            (30) NOT NULL,
+    last_name VARCHAR
+            (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY
+            (id),
+    FOREIGN KEY
+            (role_id) REFERENCES role
+            (id)
+);
--- seeds.sql
+++ seeds.sql
@@ -1,28 +1,29 @@
 INSERT INTO department
-    (name)
+    (id, name)
 VALUES
     ("Kitchen"),
     ("Front Desk"),
     ("Housekeeping"),
     ("Engineering"),
     ("Banquets"),
-    ("Human Resources")
+    ("Human Resources");
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
     ("Executive Chef", 90000, 1),
     ("Director of Housekeeping", 70000, 3),
-    ("Housekeeping Manager", 50000, 3);
+    ("Housekeeping Manager", 50000, 3),
+    ("Public Area Attendant", 35000, 3),
+    ("Cook I", 40000, 1),
+    ("Guest Service Agent", 40000, 2);
 
 INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
+    (first_name, last_name, role_id)
 VALUES
-    ("Shabina", "Khan", 3, 2);
-
-SELECT *
-FROM department;
-SELECT *
-FROM role;
-SELECT *
-FROM employee;
+    ("Shabina", "Khan", 3),
+    ("Vincent", "Alexander", 1),
+    ("Ryan", "Skog", 2),
+    ("Timote", "Loketi", 4),
+    ("Douglas", "Logan", 6),
+    ("Chris", "Troughton", 5);
